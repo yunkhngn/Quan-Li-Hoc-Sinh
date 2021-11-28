ffe42ceb8130d891d0cdac5295f84853e6b70f4e
Quang Ngo <quangngo.nz@gmail.com>
2021-11-28 22:27:25 +0700

    Add Import, Export

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Quan-Li-Hoc-Sinh
-## **Phần mềm quản lí học sinh | Students Managing Application**
-## **Creator:** *Khoa Nguyễn*
 
-*VI:*
+## **Phần mềm quản lí học sinh | Students Managing Application**
+
+## **Creator:** _Khoa Nguyễn & Quang Ngo_
+
+_VI:_
 
 Phần mềm quản lí học sinh giúp nhập thông tin học sinh vào phần mềm và xuất ra file json, đọc và truy vấn học sinh qua họ và tên, tính điểm trung bình.
 
-*EN:*
+_EN:_
 
 A tool that help managing a list of students by inputing data from keyboard and exporting into json file, read and search for data from students names and calculate average marks.
-
--- quanli.py
+++ quanli.py
@@ -6,7 +6,7 @@
 #        fuc: tính điểm trung bình 
 #             tính năm sinh 
 
-# import os
+import os
 # clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 # clearConsole()
 # #Clear console cho nó gọn
@@ -16,6 +16,8 @@ from tkinter import *
 from  tkinter import ttk
 from tkinter import messagebox
 import webbrowser
+import numpy
+import csv
 
 class Students:
     def __init__(self,stt, name, age, className, toan, van, anh):
@@ -83,31 +85,72 @@ tableData.heading("van",text="Điểm Văn",anchor=CENTER)
 tableData.heading("anh",text="Điểm Anh",anchor=CENTER)
 tableData.heading("diemTB",text="Điểm TB",anchor=CENTER)
 
-#Row 2
-data  = [
-]
 
 global count
+data = []
 count=0
+
 def openURL():
     webbrowser.open_new(r"https://www.facebook.com/fb.khoanguyen")
     messagebox.showinfo('Hỏi cl?', 'Hỏi gì? Hỏi cl tao đéo trả lời đâu!')
-def dtb():
-        return round(float(float(toan_entry.get())+float(van_entry.get())+float(anh_entry.get()))/3,2)
-def inputData():
-    
 
+def dtb():
+    return round(float(float(toan_entry.get())+float(van_entry.get())+float(anh_entry.get()))/3, 2)
+
+def inputData():
     global count
    
     tableData.insert(parent='',index='end',iid = count,text='',values=(stt_entry.get(), name_entry.get(), class_entry.get(), age_entry.get(), toan_entry.get(), van_entry.get(), anh_entry.get(), dtb()))
+    
     count += 1
+
+    data.append([stt_entry.get(), name_entry.get(), class_entry.get(), age_entry.get(), toan_entry.get(), van_entry.get(), anh_entry.get(), dtb()])
+    print(data)
+
+    stt_entry.delete(0,END)
+    name_entry.delete(0,END)
+    class_entry.delete(0,END)
+    age_entry.delete(0,END)
+    toan_entry.delete(0,END)
+    van_entry.delete(0,END)
+    anh_entry.delete(0,END)
+
+def ExportData():
+    global data
+
+    Headers = ['stt', 'name', 'class', 'age', 'toan', 'van', 'anh', 'diemTB']
+
+    dataWithHeaders = data
+    dataWithHeaders.insert(0, Headers)
+    print(dataWithHeaders)
+    dataCSV = numpy.array(dataWithHeaders)
+    with open('data.csv', 'w', newline='') as file:
+        mywriter = csv.writer(file, delimiter=',')
+        mywriter.writerows(dataCSV)
+
+def LoadData():
+    global data, tableData, count
+    count = 0
+    if os.path.exists('./data.csv'):
+        with open('data.csv', 'r', newline='') as file:
+            CSVdata = csv.reader(file, delimiter=',')
+            for rows in CSVdata:
+                data.append(rows)
+            data.pop(0)
+        
+        for i in data:
+            tableData.insert(parent='',index='end',iid = count,text='',values=(i[0], i[1],i[2] ,i[3], i[4], i[5], i[6], i[7]))
+            count +=1
+    else:
+        return
+
 def deleteData():
     messagebox.askyesno('Ditmemay ấn cl', 'Cái này chưa code ấn cl à, code giúp tao với?')
 
 Input_frame = Frame(screen)
 Input_frame.grid(columnspan = "13", row = "2", padx=(20,0), pady=0)
 
-menuLabel = Label(Input_frame, text = "MENU", font=("Segoe UI Bold",10))
+menuLabel = Label(Input_frame, text = "Menu", font=("Segoe UI Bold",15))
 menuLabel.grid(columnspan = "13", row = "2", padx=5, pady=5)
 
 stt = Label(Input_frame,text="STT")
@@ -152,16 +195,19 @@ van_entry.grid(row=4,column=5,padx=5,pady=0)
 anh_entry = Entry(Input_frame,borderwidth=2, width="7", font=("Segoe UI",12), justify="center", relief="groove")
 anh_entry.grid(row=4,column=6,padx=5,pady=0)
 
-Input_button = Button(Input_frame,width = 10, text = "Import",command= inputData, font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
-Input_button.grid(row=4,column=7,columnspan=2,padx=5,pady=0)
+Add_button = Button(Input_frame, text="Add", font=("Segoe UI",12), command=inputData, relief=GROOVE, justify="center", bd=2,width = 10)
+Add_button.grid(row=4,column=7,columnspan=2,padx=0,pady=5)
 
-Export_button = Button(Input_frame,width = 10, text = "Export", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
+Export_button = Button(Input_frame, width = 10, text = "Export", command = ExportData, font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
 Export_button.grid(row=5,column=7,columnspan=2,padx=0,pady=5)
 
-Search_button = Button(Input_frame,width = 10, text = "Search", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2, command=deleteData)
+Input_button = Button(Input_frame,width = 10, text = "Import",command= LoadData, font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
+Input_button.grid(row=6,column=7,columnspan=2,padx=5,pady=0)
+
+Search_button = Button(Input_frame,width = 10, text = "Search", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2, command = deleteData)
 Search_button.grid(row=5,column=5,columnspan=2,padx=0,pady=5)
 
-Clear_button = Button(Input_frame,width = 10, text = "Clear", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2, command=deleteData)
+Clear_button = Button(Input_frame,width = 10, text = "Clear", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2, command = deleteData)
 Clear_button.grid(row=5,column=3,columnspan=3,padx=0,pady=5)
 
 Gethelp_button = Button(Input_frame,width = 10, text = "Get help?", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2, command=openURL)
@@ -170,5 +216,5 @@ Gethelp_button.grid(row=5,column=2,columnspan=2,padx=0,pady=5)
 #CopyRight Label
 copyRightLabel = Label(Input_frame)
 copyRightLabel.grid(columnspan = "2", column = 0, row = "5", padx=(35,0), pady=(0,5))
-copyRightLabel.config(text="Copyright © Khoa Nguyễn! For educational purposes only!", font=("Segoe UI light",10),bd="0", relief=GROOVE, justify="center")
+copyRightLabel.config(text="Copyright © Khoa Nguyễn & Quang Ngo! For educational purposes only!", font=("Segoe UI light",10),bd="0", relief=GROOVE, justify="center")
 screen.mainloop()

b0799c1289bfd62c4acbeb5f61e7455a1f4edf4e
ROGKhoa <khoathientai2005@gmail.com>
2021-11-28 21:26:03 +0700

    Improved & added some function

--- quanli.py
+++ quanli.py
@@ -14,7 +14,8 @@
 import datetime
 from tkinter import *
 from  tkinter import ttk
-
+import json
+import webbrowser
 
 class Students:
     def __init__(self,stt, name, age, className, toan, van, anh):
@@ -38,57 +39,9 @@ class Students:
 
 HocSinh = []
 
-def NhapHocSinh():
-    SoHocSinh = int(input('So hoc sinh: '))
-
-    for i in range(SoHocSinh):
-        STT = i+1
-        Ten = str(input("Nổ cái tên: "))
-        Tuoi = int(input("Tuổi? :"))
-        Lop = str(input("Học lớp nào???: "))
-        Toan = float(input("Điểm toán:  "))
-        Van = float(input("Điểm văn: "))
-        Anh = float(input("Điểm anh: "))
-        print("")
-
-        HocSinh.append(Students(STT, Ten, Tuoi, Lop, Toan,Van, Anh))
-
-def LayHocSinh():
-    HocSinhMuonCheck = int(input("Nhập số thứ tự: ")) - 1
-    print("")
-    print("Muốn lấy thông tin nào: ")
-    print("[1] Tên")
-    print("[2] Tuổi")
-    print("[3] Lớp")
-    print("[4] Điểm trung bình")
-    print("[5] Năm sinh")
-    option = int(input("Lựa chọn của bạn: "))
-    print("")
-
-    if option == 1:
-        print(HocSinh[HocSinhMuonCheck].name)
-    if option == 2:
-        print(HocSinh[HocSinhMuonCheck].age)
-    if option == 3:
-        print(HocSinh[HocSinhMuonCheck].className)
-    if option == 4:
-        print(HocSinh[HocSinhMuonCheck].tinhTrungBinh())
-    if option == 5:
-        print(HocSinh[HocSinhMuonCheck].tinhNamSinh())
-
-# while True:
-#     print("[1] Nhập học sinh [2] Lấy học sinh")
-#     luaChon = int(input("Lựa chọn của bạn: "))
-
-#     if luaChon == 1:
-#         NhapHocSinh()
-#     if luaChon == 2:
-#         LayHocSinh()
-
 screen = Tk()
-screen.geometry("950x500")
-screen.maxsize(950, 450)
-screen.minsize(950, 450)
+screen.geometry("950x450")
+screen. resizable(width=False, height=False)
 screen.title("Phần mềm quản lí học sinh trường L")
 
 #Row 0
@@ -98,12 +51,16 @@ titleLabel.config(text = "QUẢN LÍ HỌC SINH", font=("Segoe UI bold",15))
 
 #Table row 1
 
-game_frame = Frame(screen)
-game_frame.grid(columnspan = "13", row = "1", padx=(30,0), pady=5)
+table_frame = Frame(screen)
+table_frame.grid(columnspan = "13", row = "1", padx=(30,0), pady=5)
 
-tableData = ttk.Treeview(game_frame)
+tableScroll = Scrollbar(table_frame)
+tableScroll.grid(row = "1", column = "8", sticky = "nsew")
+
+tableData = ttk.Treeview(table_frame)
 
 tableData['columns'] = ('stt', 'name', 'class', 'age', 'toan', 'van', 'anh', 'diemTB')
+tableData.grid(row=1, column=0, columnspan=5, rowspan=5, padx=5, pady=5)
 
 tableData.column("#0", width=0,  stretch=NO)
 tableData.column("stt",anchor=CENTER, width=70)
@@ -126,92 +83,98 @@ tableData.heading("van",text="Điểm Văn",anchor=CENTER)
 tableData.heading("anh",text="Điểm Anh",anchor=CENTER)
 tableData.heading("diemTB",text="Điểm TB",anchor=CENTER)
 
-tableData.insert(parent='', index='end', iid=0, text='',
-values=(1, 'Nguyễn Minh Đức', 'Lớp 11D0', '12', '10', '10', '10', '10'))
-
-def insertData():
-    stt =sttEntry.get()
-    HocSinh[stt].name = nameEntry.get()
-    HocSinh[stt].age = ageEntry.get()
-    HocSinh[stt].className = classEntry.get()
-    HocSinh[stt].toan = toanEntry.get()
-    HocSinh[stt].van = vanEntry.get()
-    HocSinh[stt].anh = anhEntry.get()
-    HocSinh[stt].tinhTrungBinh()
-    tableData.insert(parent='',index='end',iid=stt,text='',
-    values=(HocSinh[stt].stt, str(HocSinh[stt].name), HocSinh[stt].className, HocSinh[stt].age, HocSinh[stt].toan, HocSinh[stt].van, HocSinh[stt].anh, HocSinh[stt].tinhTrungBinh()))
-
-tableData.grid(row=1, column=0, columnspan=5, rowspan=5, padx=5, pady=5)
-        
 #Row 2
-searchButton = Button(screen, width=7)
-searchButton.grid(column = "0", row = "2", padx=(35,0), pady=(7,0))
-searchButton.config(text="Search", font=("Segoe UI",12), relief=GROOVE, justify="center")
+data  = [
+]
 
-sttEntry = Entry(screen, width= "7")
-sttEntry.grid(column = "1", row = "2", padx=0, pady=(7,0))
-sttEntry.config(text="Stt", font=("Segoe UI",12), relief=GROOVE, justify="center")
+global count
+count=0
+def openURL():
+    webbrowser.open_new(r"https://www.facebook.com/fb.khoanguyen")
+def dtb():
+        return round(float(float(toan_entry.get())+float(van_entry.get())+float(anh_entry.get()))/3,2)
+def inputData():
+    
 
-nameEntry = Entry(screen, width= "25")
-nameEntry.grid(column = "2", row = "2", padx=0, pady=(7,0))
-nameEntry.config(text="Tên", font=("Segoe UI",12), relief=GROOVE, justify="center")
+    global count
+   
+    tableData.insert(parent='',index='end',iid = count,text='',values=(stt_entry.get(), name_entry.get(), class_entry.get(), age_entry.get(), toan_entry.get(), van_entry.get(), anh_entry.get(), dtb()))
+    count += 1
 
-ageEntry = Entry(screen, width= 6)
-ageEntry.grid(column = "3", row = "2", padx=0, pady=(7,0))
-ageEntry.config(text="Tuổi", font=("Segoe UI",12), relief=GROOVE, justify="center")
+   
+    stt_entry.delete(0,END)
+    name_entry.delete(0,END)
+    class_entry.delete(0,END)
+    age_entry.delete(0,END)
+    toan_entry.delete(0,END)
+    van_entry.delete(0,END)
+    anh_entry.delete(0,END)
 
-classEntry = Entry(screen, width= 15)
-classEntry.grid(column = "4", row = "2", padx=0, pady=(7,0))
-classEntry.config(text="Lớp", font=("Segoe UI",12), relief=GROOVE, justify="center")
+Input_frame = Frame(screen)
+Input_frame.grid(columnspan = "13", row = "2", padx=(20,0), pady=0)
 
-toanEntry = Entry(screen, width= 10)
-toanEntry.grid(column = "5", row = "2", padx=0, pady=(7,0))
-toanEntry.config(text="Toán", font=("Segoe UI",12), relief=GROOVE, justify="center")
+menuLabel = Label(Input_frame, text = "Menu", font=("Segoe UI Bold",15))
+menuLabel.grid(columnspan = "13", row = "2", padx=5, pady=5)
 
-vanEntry = Entry(screen, width= 10)
-vanEntry.grid(column = "6", row = "2", padx=0, pady=(7,0))
-vanEntry.config(text="Văn", font=("Segoe UI",12), relief=GROOVE, justify="center")
+stt = Label(Input_frame,text="STT")
+stt.grid(row=3,column=0,padx=5,pady=0)
 
-anhEntry = Entry(screen, width= 10)
-anhEntry.grid(column = "7", row = "2", padx=0, pady=(7,0))
-anhEntry.config(text="Anh", font=("Segoe UI",12), relief=GROOVE, justify="center")
+name = Label(Input_frame,text="Tên")
+name.grid(row=3,column=1,padx=5,pady=0)
 
-#Row 3
-exportLabel = Button(screen, width= "7")
-exportLabel.grid(column = "0", row = "3", padx=(35,0), pady=(7,0))
-exportLabel.config(text="Clear", font=("Segoe UI",12), relief=GROOVE, justify="center")
+className = Label(Input_frame,text="Lớp")
+className.grid(row=3,column=2,padx=5,pady=0)
 
-inputButton = Button(screen, width=7,)
-inputButton.grid(column = "1", row = "3", padx=0, pady=(7,0))
-inputButton.config(text="Input", font=("Segoe UI",12), relief=GROOVE, justify="center", command = insertData)
+age = Label(Input_frame,text="Tuổi")
+age.grid(row=3,column=3,padx=5,pady=0)
 
-nameLabel = Label(screen, width= "25")
-nameLabel.grid(column = "2", row = "3", padx=0, pady=(7,0))
-nameLabel.config(text="Tên", font=("Segoe UI",12), relief=GROOVE, justify="center")
+toan = Label(Input_frame,text="Điểm Toán")
+toan.grid(row=3,column=4,padx=5,pady=0)
 
-ageLabel = Label(screen, width= 6)
-ageLabel.grid(column = "3", row = "3", padx=0, pady=(7,0))
-ageLabel.config(text="Tuổi", font=("Segoe UI",12), relief=GROOVE, justify="center")
+van = Label(Input_frame,text="Điểm Văn")
+van.grid(row=3,column=5,padx=5,pady=0)
 
-classLabel = Label(screen, width= 15)
-classLabel.grid(column = "4", row = "3", padx=0, pady=(7,0))
-classLabel.config(text="Lớp", font=("Segoe UI",12), relief=GROOVE, justify="center")
+anh = Label(Input_frame,text="Điểm Anh")
+anh.grid(row=3,column=6,padx=5,pady=0)
 
-toanLabel = Label(screen, width= 10)
-toanLabel.grid(column = "5", row = "3", padx=0, pady=(7,0))
-toanLabel.config(text="Toán", font=("Segoe UI",12), relief=GROOVE, justify="center")
+stt_entry = Entry(Input_frame, borderwidth=2, width = "6", font=("Segoe UI",12), justify="center", relief="groove")
+stt_entry.grid(row=4,column=0,padx=3,pady=0)
 
-vanLabel = Label(screen, width= 10)
-vanLabel.grid(column = "6", row = "3", padx=0, pady=(7,0))
-vanLabel.config(text="Văn", font=("Segoe UI",12), relief=GROOVE, justify="center")
+name_entry = Entry(Input_frame,borderwidth=2, width = "33", font=("Segoe UI",12), justify="right", relief="groove")
+name_entry.grid(row=4,column=1,padx=5,pady=0)
 
-anhLabel = Label(screen, width= 10)
-anhLabel.grid(column = "7", row = "3", padx=0, pady=(7,0))
-anhLabel.config(text="Anh", font=("Segoe UI",12), relief=GROOVE, justify="center")
+class_entry = Entry(Input_frame,borderwidth=2, width = "7", font=("Segoe UI",12), justify="center", relief="groove")
+class_entry.grid(row=4,column=2,padx=5,pady=0)
+
+age_entry = Entry(Input_frame,borderwidth=2, width="7", font=("Segoe UI",12), justify="center", relief="groove")
+age_entry.grid(row=4,column=3,padx=5,pady=0)
+
+toan_entry = Entry(Input_frame,borderwidth=2, width="7", font=("Segoe UI",12), justify="center", relief="groove")
+toan_entry.grid(row=4,column=4,padx=5,pady=0)
+
+van_entry = Entry(Input_frame,borderwidth=2, width="7", font=("Segoe UI",12), justify="center", relief="groove")
+van_entry.grid(row=4,column=5,padx=5,pady=0)
+
+anh_entry = Entry(Input_frame,borderwidth=2, width="7", font=("Segoe UI",12), justify="center", relief="groove")
+anh_entry.grid(row=4,column=6,padx=5,pady=0)
+
+Input_button = Button(Input_frame,width = 10, text = "Import",command= inputData, font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
+Input_button.grid(row=4,column=7,columnspan=2,padx=5,pady=0)
+
+Export_button = Button(Input_frame,width = 10, text = "Export", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
+Export_button.grid(row=5,column=7,columnspan=2,padx=0,pady=5)
+
+Search_button = Button(Input_frame,width = 10, text = "Search", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
+Search_button.grid(row=5,column=5,columnspan=2,padx=0,pady=5)
+
+Clear_button = Button(Input_frame,width = 10, text = "Clear", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2)
+Clear_button.grid(row=5,column=3,columnspan=3,padx=0,pady=5)
+
+Gethelp_button = Button(Input_frame,width = 10, text = "Get help?", font=("Segoe UI",12), relief=GROOVE, justify="center", bd=2, command=openURL)
+Gethelp_button.grid(row=5,column=2,columnspan=2,padx=0,pady=5)
 
 #CopyRight Label
-copyRightLabel = Label(screen)
-copyRightLabel.grid(columnspan = "13", row = "4", padx=(35,0), pady=(30,0))
-copyRightLabel.config(text="Copyright © Khoa Nguyễn, supported by Copilot!", font=("Segoe UI light",10),bd="0", relief=GROOVE, justify="center")
-
+copyRightLabel = Label(Input_frame)
+copyRightLabel.grid(columnspan = "2", column = 0, row = "5", padx=(35,0), pady=(0,5))
+copyRightLabel.config(text="Copyright © Khoa Nguyễn! For educational purposes only!", font=("Segoe UI light",10),bd="0", relief=GROOVE, justify="center")
 screen.mainloop()
